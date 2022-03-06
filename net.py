7f863a97a0518c6d4a27391b573a4e3e3b419c9f
brk9009 <warsend23@hotmail.com>
2022-03-05 18:07:50 -0600

    get the content of a website

--- net.py
+++ net.py
@@ -1,1 +1,11 @@
-k
+import requests
+
+# Making a GET request
+r = requests.get('https://www.warrennolan.com/basketball/2022/team-net-sheet?team=Jacksonville-State')
+
+# check status code for response received
+# success code - 200
+print(r.status_code)
+
+# print content of request
+print(r.content)
