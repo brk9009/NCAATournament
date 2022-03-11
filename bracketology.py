1d117477325be9bafd26ddde1f108146b1119e63
ben kixmiller <benkixmiller@localhost.localdomain>
2022-03-11 04:16:17 -0500

    Move teams list to new file and new code

--- bracketology.py
+++ bracketology.py
@@ -2,11 +2,15 @@
 
 import requests
 from bs4 import BeautifulSoup
+import csv
 
-URL = "https://www.warrennolan.com/basketball/2022/team-net-sheet?team=Bellarmine"
+from teams import team_links
+
+#urls = ["https://www.warrennolan.com/basketball/2022/team-net-sheet?team=Bellarmine"]
 
 # Get the HTML data
-page = requests.get(URL)
+for url in team_links:
+    page = requests.get(url)
 
 # print(page.text)
 # Create a BeautifulSoup object that uses the html parser to parse the page.
@@ -17,9 +21,15 @@ websiteInfo = soup.find(id="container-x")
 #print(websiteInfo.prettify())
 
 # Get the School name, conference, NET, record, and SoS
-basicSchoolInfoList = websiteInfo.find_all("div", class_="ts-flex-size-1")
+#basicSchoolInfoList = websiteInfo.find_all("div", class_="ts-flex-size-1")
 
-for basicSchoolInfo in basicSchoolInfoList:
-    print(basicSchoolInfo.prettify())
+#for basicSchoolInfo in basicSchoolInfoList:
+#    print(basicSchoolInfo.prettify())
 
+teamName = websiteInfo.find("div", class_="ts-teamname")
+print(teamName.text)
+
+teamInfoList = websiteInfo.find_all("div", class_="ts-data-center")
+for teamInfo in teamInfoList:
+    print(teamInfo.text)
 
---
+++ teams.py
@@ -0,0 +1,2 @@
+team_urls = ['https://www.warrennolan.com/basketball/2022/team-net-sheet?team=Bellarmine']
+
