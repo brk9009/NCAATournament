b6419d0aa3c76fa40074e80f0930a9dc4016cbce
ben kixmiller <benkixmiller@localhost.localdomain>
2022-03-11 02:47:03 -0500

    Parse net sheet information

--- bracketology.py
+++ bracketology.py
@@ -5,7 +5,19 @@ from bs4 import BeautifulSoup
 
 URL = "https://www.warrennolan.com/basketball/2022/team-net-sheet?team=Bellarmine"
 
+# Get the HTML data
 page = requests.get(URL)
 
 # print(page.text)
+# Create a BeautifulSoup object that uses the html parser to parse the page.
 soup = BeautifulSoup(page.content, "html.parser")
+
+# Find the team's information from the "container-x" id
+websiteInfo = soup.find(id="container-x")
+#print(websiteInfo.prettify())
+
+# Find the net sheet from the "ts-container" class in "container-x"
+netSheet = websiteInfo.select_one(".ts-container")
+print(netSheet.prettify())
+
+
