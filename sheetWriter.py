3d44d09a41a67fb80c8dc196642c7ce4725efbcc
ben kixmiller <benkixmiller@localhost.localdomain>
2022-03-12 14:46:29 -0500

    Add formatting

--- sheetWriter.py
+++ sheetWriter.py
@@ -10,6 +10,7 @@ class SheetWriter():
     def write_to_excel(self):
         """Write Information to Excel file"""
         self.create_workbook()
+        self.add_formatting()
         self.write_headers()
         self.write_data()
 
@@ -17,23 +18,33 @@ class SheetWriter():
         """Create a workbook and add a worksheet"""
         self.workbook = xlsxwriter.Workbook('bracketology.xlsx')
         self.worksheet = self.workbook.add_worksheet('Portfolios')
+        # Protect from unwanted edits
+
+    def add_formatting(self):
+        """Add formatting to the workbook"""
+        # Set the header format
+        self.header_format = self.workbook.add_format({
+            'bold': True, 'font_name': 'Times New Roman', 'bg_color': 'red', 'font_color': 'yellow'})
+        # Set the data format
+        self.data_format = self.workbook.add_format({
+            'font_name': 'Times New Roman', 'bg_color': 'silver'})
 
     def write_headers(self):
         """Prepare the headers for the file"""
         # Write the data headers
-        self.worksheet.write('A1', 'Team')
-        self.worksheet.write('B1', 'Conf.')
-        self.worksheet.write('C1', 'Record')
-        self.worksheet.write('D1', 'NET')
-        self.worksheet.write('E1', 'KevPauga')
-        self.worksheet.write('F1', 'ESPN SoR')
-        self.worksheet.write('G1', 'BPI')
-        self.worksheet.write('H1', 'KenPom')
-        self.worksheet.write('I1', 'Sagarin')
-        self.worksheet.write('J1', 'Quad1')
-        self.worksheet.write('K1', 'Quad2')
-        self.worksheet.write('L1', 'Quad3')
-        self.worksheet.write('M1', 'Quad4')
+        self.worksheet.write('A1', 'Team', self.header_format)
+        self.worksheet.write('B1', 'Conf.', self.header_format)
+        self.worksheet.write('C1', 'Record', self.header_format)
+        self.worksheet.write('D1', 'NET', self.header_format)
+        self.worksheet.write('E1', 'KevPauga', self.header_format)
+        self.worksheet.write('F1', 'ESPN SoR', self.header_format)
+        self.worksheet.write('G1', 'BPI', self.header_format)
+        self.worksheet.write('H1', 'KenPom', self.header_format)
+        self.worksheet.write('I1', 'Sagarin', self.header_format)
+        self.worksheet.write('J1', 'Quad1', self.header_format)
+        self.worksheet.write('K1', 'Quad2', self.header_format)
+        self.worksheet.write('L1', 'Quad3', self.header_format)
+        self.worksheet.write('M1', 'Quad4', self.header_format)
 
     def write_data(self):
         """Write the data from bracketology.py to an excel sheet"""
@@ -45,7 +56,7 @@ class SheetWriter():
         for contenderData in self.contendersData:
             col = 0
             for metric in contenderData:
-                self.worksheet.write_string(row, col, metric)
+                self.worksheet.write_string(row, col, metric, self.data_format)
                 col += 1
 
             row +=1
